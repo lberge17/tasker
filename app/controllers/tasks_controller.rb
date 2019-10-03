d72c96ace1ad885943cf1a7e5a8614171e40f967
Laura Berge <laura@Lauras-MacBook-Pro-2.local>
2019-10-03 17:31:27 -0400

    set up the tasks controller to have task routes under a given group

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,30 +1,37 @@
 class TasksController < ApplicationController
 
-  get "/tasks" do
-    erb :"/tasks/index.html"
-  end
+#  get "/groups/:slug/tasks" do
+#    @group = Group.find_by_slug(params[:slug])
+#    erb :"/tasks/index.html"
+#  end
 
-  get "/tasks/new" do
-    erb :"/tasks/new.html"
-  end
+#  get "/groups/:slug/tasks/new" do
+#    @group = Group.find_by_slug(params[:slug])
+#    erb :"/tasks/new.html"
+#  end
 
-  post "/tasks" do
-    redirect "/tasks"
-  end
+#  post "/groups/:slug/tasks" do
+#    @group = Group.find_by_slug(params[:slug])
+#    redirect "/tasks"
+#  end
 
-  get "/tasks/:id" do
-    erb :"/tasks/show.html"
-  end
+#  get "/groups/:slug/tasks/:id" do
+#    @group = Group.find_by_slug(params[:slug])
+#    erb :"/tasks/show.html"
+#  end
 
-  get "/tasks/:id/edit" do
-    erb :"/tasks/edit.html"
-  end
+#  get "/groups/:slug/tasks/:id/edit" do
+#    @group = Group.find_by_slug(params[:slug])
+#    erb :"/tasks/edit.html"
+#  end
 
-  patch "/tasks/:id" do
-    redirect "/tasks/:id"
-  end
+#  patch "/groups/:slug/tasks/:id" do
+#    @group = Group.find_by_slug(params[:slug])
+#    redirect "/tasks/:id"
+#  end
 
-  delete "/tasks/:id/delete" do
-    redirect "/tasks"
-  end
+#  delete "/groups/:slug/tasks/:id/delete" do
+#    @group = Group.find_by_slug(params[:slug])
+#    redirect "/tasks"
+#  end
 end
