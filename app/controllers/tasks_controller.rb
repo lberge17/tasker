35d449de554ab1e33977e0a9b1c20ad2d61041b6
Laura Berge <laura@Lauras-MacBook-Pro-2.local>
2019-10-03 18:49:37 -0400

    now groups can create new tasks

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,19 +1,41 @@
 class TasksController < ApplicationController
 
-#  get "/groups/:slug/tasks" do
-#    @group = Group.find_by_slug(params[:slug])
-#    erb :"/tasks/index.html"
-#  end
+  get "/groups/:slug/tasks" do
+    @group = Group.find_by_slug(params[:slug])
+    if logged_in? && @group.users.include?(current_user)
+      erb :"/tasks/index.html"
+    else
+      redirect '/'
+    end
+  end
 
   get "/groups/:slug/tasks/new" do
     @group = Group.find_by_slug(params[:slug])
-    erb :"/tasks/new.html"
+    if logged_in? && @group.users.include?(current_user)
+      erb :"/tasks/new.html"
+    else
+      redirect '/'
+    end
   end
 
-#  post "/groups/:slug/tasks" do
-#    @group = Group.find_by_slug(params[:slug])
-#    redirect "/tasks"
-#  end
+  post "/groups/:slug/tasks" do
+    group = Group.find_by_slug(params[:slug])
+    if !params["title"].empty?
+      task = Task.create(title: params["title"], complete?: false)
+      if !params["todo_1"].empty?
+        task.sub_tasks << SubTask.create(content: params["todo_1"], complete?: false)
+      end
+      if !params["todo_2"].empty?
+        task.sub_tasks << SubTask.create(content: params["todo_2"], complete?: false)
+      end
+      if !params["todo_3"].empty?
+        task.sub_tasks << SubTask.create(content: params["todo_3"], complete?: false)
+      end
+      task.group = group
+      task.save
+    end
+    redirect "/groups/#{group.slug}/tasks"
+  end
 
 #  get "/groups/:slug/tasks/:id" do
 #    @group = Group.find_by_slug(params[:slug])
