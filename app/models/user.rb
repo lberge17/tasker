a36850b5d2bff160db9d4f15ef505c932f28d734
Laura Berge <laura@Lauras-MacBook-Pro-2.local>
2019-10-03 11:58:13 -0400

    add validations for email and username uniqueness

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
+  validates :email, uniqueness: true
+  validates :username, uniqueness: true
+
   has_many :group_users
   has_many :groups, through: :group_users
   has_many :tasks, through: :groups
