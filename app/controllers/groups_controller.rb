315ede43b7ef87ef203cacd5256139feba8a55f3
Laura Berge <laura@Lauras-MacBook-Pro-2.local>
2019-10-05 11:24:16 -0400

    Added ability to transfer ownership of a group and edit a group--still need to fix some bugs on this with members and owners

--- app/controllers/groups_controller.rb
+++ app/controllers/groups_controller.rb
@@ -43,15 +43,35 @@ class GroupsController < ApplicationController
     redirect "/groups/#{group.slug}"
   end
 
-#  get "/groups/:id/edit" do
-#    erb :"/groups/edit.html"
-#  end
+  get "/groups/:slug/edit" do
+    @group = Group.find_by_slug(params[:slug])
+    if logged_in? && (@group.owner == current_user)
+      erb :"/groups/edit.html"
+    else
+      redirect '/groups'
+    end
+  end
 
-#  patch "/groups/:id" do
-#    redirect "/groups/:id"
-#  end
+  patch "/groups/:slug" do
+    group = Group.find_by_slug(params[:slug])
+    user = User.find_by(username: params["username"])
 
-#  delete "/groups/:id/delete" do
+    group.update(name: params["name"]) if !params["name"].empty?
+
+    if !params["username"].empty? && user
+      group.owner = user
+      group.save
+      group.members << current_user if (group.owner != current_user)
+
+      group.members.delete(User.find(user))
+
+      group.save
+    end
+
+    redirect "/groups/#{group.slug}"
+  end
+
+#  delete "/groups/:slug/delete" do
 #    redirect "/groups"
 #  end
 end
